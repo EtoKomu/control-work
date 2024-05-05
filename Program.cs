a6de612c9c1cdaf528e88d1d03c19f28e0103846
EtoKomu <peepoetokomu@gmail.com>
2024-05-05 19:02:42 +0500

    printArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+using System;
+
+class Program  
+{
+    static void Main()
+    {
+        string[] array = ["peepo", "135", "rebyata", "9", "che"];
+    }
+    static void printArray(string[] array)
+    {
+        System.Console.Write("[");
+        for (int i = 0; i < array.Length-1; i++)
+        {
+            System.Console.Write($"{array[i]}, ");
+        }
+        System.Console.Write($"{array[array.Length-1]}]");
+    }
+}
